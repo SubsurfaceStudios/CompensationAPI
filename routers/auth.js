04375a5af353b6e3645adb181b83235d064e7fb1
bubby932 <tvrec10@gmail.com>
2022-01-15 14:43:19 -0500

    lol idiot

--- routers/auth.js
+++ routers/auth.js
@@ -4,6 +4,8 @@ const middleware = require('../middleware');
 const fs = require('fs');
 const BadWordList = JSON.parse(fs.readFileSync('./data/external/badwords-master/array.json'));
 const sanitize = require('sanitize-filename');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 //Call to get a token from user account credentials.
 router.post("/login", (req, res) => {

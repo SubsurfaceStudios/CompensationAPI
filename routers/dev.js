823cda5398db8dcf5e338ff146fe9fb59f673e8f
bubby932 <tvrec10@gmail.com>
2022-01-19 14:16:16 -0500

    it didn't work 2 electric boogaloo

--- routers/dev.js
+++ routers/dev.js
@@ -10,25 +10,4 @@ router.get("/check", middleware.authenticateDeveloperToken, async (req, res) => 
      return res.status(200).send("This token is verified as Developer.");
 });
 
-router.post("/play-sound", middleware.authenticateDeveloperToken, async (req, res) => {
-     var {url} = req.body;
-     if(typeof url !== 'string') return res.status(400).send("You failed to specify a URL!");
-
-     var count = helpers.getAccountCount();
-     for (let index = 0; index < count; index++) {
-          require('../index').sendStringToClient(index.toString(), `PLAY_SOUND ${url}`);
-     }
-
-     res.sendStatus(200);
-});
-
-router.post("/stop-sounds", middleware.authenticateDeveloperToken, async (req, res) => {
-     var count = helpers.getAccountCount();
-     for (let i = 0; i < count; i++) {
-          require('../index').sendStringToClient(index.toString(), "STOP_SOUND")
-     }
-
-     res.sendStatus(200);
-});
-
 module.exports = router;

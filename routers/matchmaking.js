47baa36f33b5279fe3d0243e9543ea059e71b53b
Rose <82341739+bubby932@users.noreply.github.com>
2022-02-08 11:56:03 -0500

    Pull changes into production. (#80)
    * Add Firebase support
    
    * The return of image support.
    
    * Minor change
    
    * Pull image support into main. (#70)
    
    * The return of image support.
    
    * Minor change
    
    * Lock image uploads.
    
    Locked image uploads to Developers only in preperation for production pull.
    
    * Pull changes into main. (#73)
    
    * Pull changes into development (#71)
    
    * Pull image support into main. (#70)
    
    * The return of image support.
    
    * Minor change
    
    * Lock image uploads.
    
    Locked image uploads to Developers only in preperation for production pull.
    
    * Important updates to 2FA.
    
    * Pull changes into main. (#75)
    
    * Pull changes into development (#71)
    
    * Pull image support into main. (#70)
    
    * The return of image support.
    
    * Minor change
    
    * Lock image uploads.
    
    Locked image uploads to Developers only in preperation for production pull.
    
    * Important updates to 2FA.
    
    * facepalm
    
    * Pull placeholders into main. (#77)
    
    * Pull changes into development (#71)
    
    * Pull image support into main. (#70)
    
    * The return of image support.
    
    * Minor change
    
    * Lock image uploads.
    
    Locked image uploads to Developers only in preperation for production pull.
    
    * Important updates to 2FA.
    
    * facepalm
    
    * Placeholder implementation.
    
    Added placeholder wildcard endpoints for
    /api/matchmaking/* and
    /api/rooms/*
    
    * Bump ws from 8.4.2 to 8.5.0 (#78)
    
    Bumps [ws](https://github.com/websockets/ws) from 8.4.2 to 8.5.0.
    - [Release notes](https://github.com/websockets/ws/releases)
    - [Commits](https://github.com/websockets/ws/compare/8.4.2...8.5.0)
    
    ---
    updated-dependencies:
    - dependency-name: ws
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ routers/matchmaking.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const helpers = require('../helpers');
+const middleware = require('../middleware');
+const fs = require('fs');
+const BadWordList = JSON.parse(fs.readFileSync('./data/external/badwords-master/array.json'));
+const sanitize = require('sanitize-filename');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+router.get("/*", async (req, res) => {
+     res.sendStatus(501);
+});
+
+module.exports = router;

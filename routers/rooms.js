d22a2c2c500588992aef33669f205ebd6d96a37b
bubby932 <tvrec10@gmail.com>
2022-02-19 10:24:07 -0500

    Implemented more room info endpoints.
    * /api/rooms/:id/user-permissions
    * /api/rooms/:id/permissions-table

--- routers/rooms.js
+++ routers/rooms.js
@@ -47,7 +47,7 @@ router.get("/:id/public-info", middleware.authenticateDeveloperToken, async (req
           var room = await collection.findOne({_id: id});
           if(typeof room == 'undefined') return res.status(404).send({message: "Room does not exist or cannot be read from the database."});
 
-          if(room.metadata.permissions[req.user.id] == 0 || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.public);
+          if(room.metadata.permissions[req.user.id] == "owner" || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.public);
 
           var permission = "everyone";
           if(room.metadata.permissions.includes(req.user.id)) permission = room.metadata.permissions[req.user.id];
@@ -62,6 +62,80 @@ router.get("/:id/public-info", middleware.authenticateDeveloperToken, async (req
      }
 });
 
+router.get("/:id/user-permissions", middleware.authenticateDeveloperToken, async (req, res) => {
+     try {
+          var {id} = req.params;
+          const playerData = helpers.PullPlayerData(req.user.id);
 
+          try {
+               id = parseInt(id);
+               if(isNaN(id)) return res.status(400).send({message: "Invalid room ID specifed."});
+          } catch {
+               return res.status(500).send({message: "Failed to parse room ID."});
+          }
+
+          const db = require('../index').mongoClient.db(process.env.MONGOOSE_DATABASE_NAME);
+          var collection = db.collection("configuration");
+
+          const RoomCount = await collection.findOne({_id: "RoomCount"}).count;
+          if(id > RoomCount || id < 0) return res.status(400).send({message: "Room does not exist."});
+
+          collection = db.collection("rooms");
+
+          var room = await collection.findOne({_id: id});
+          if(typeof room == 'undefined') return res.status(404).send({message: "Room does not exist or cannot be read from the database."});
+
+          if(room.metadata.permissions[req.user.id] == "owner" || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.metadata.permissions);
+
+          var permission = "everyone";
+          if(room.metadata.permissions.includes(req.user.id)) permission = room.metadata.permissions[req.user.id];
+
+          const playerPermissionsTable = room.metadata.permissionTable[permission];
+
+          if(!playerPermissionsTable.editPermissions) return res.status(403).send({message: "You do not have permission to view information about this room."});
+
+          res.status(200).json(room.metadata.permissions);
+     } catch {
+          res.sendStatus(500);
+     }
+});
+
+router.get("/:id/permissions-table", middleware.authenticateDeveloperToken, async (req, res) => {
+     try {
+          var {id} = req.params;
+          const playerData = helpers.PullPlayerData(req.user.id);
+
+          try {
+               id = parseInt(id);
+               if(isNaN(id)) return res.status(400).send({message: "Invalid room ID specifed."});
+          } catch {
+               return res.status(500).send({message: "Failed to parse room ID."});
+          }
+
+          const db = require('../index').mongoClient.db(process.env.MONGOOSE_DATABASE_NAME);
+          var collection = db.collection("configuration");
+
+          const RoomCount = await collection.findOne({_id: "RoomCount"}).count;
+          if(id > RoomCount || id < 0) return res.status(400).send({message: "Room does not exist."});
+
+          collection = db.collection("rooms");
+
+          var room = await collection.findOne({_id: id});
+          if(typeof room == 'undefined') return res.status(404).send({message: "Room does not exist or cannot be read from the database."});
+
+          if(room.metadata.permissions[req.user.id] == "owner" || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.metadata.permissionTable);
+
+          var permission = "everyone";
+          if(room.metadata.permissions.includes(req.user.id)) permission = room.metadata.permissions[req.user.id];
+
+          const playerPermissionsTable = room.metadata.permissionTable[permission];
+
+          if(!playerPermissionsTable.editPermissions) return res.status(403).send({message: "You do not have permission to view information about this room."});
+
+          res.status(200).json(room.metadata.permissionTable);
+     } catch {
+          res.sendStatus(500);
+     }
+});
 
 module.exports = router;

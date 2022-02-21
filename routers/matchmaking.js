9503ed6c4461482f932c5e4931bb2cffd77b620c
Rose <82341739+bubby932@users.noreply.github.com>
2022-02-21 15:56:32 -0500

    Pull all changes into production. (#117)
    * Update image retrieval endpoint.
    
    It now uses direct transfer between the database and the response,
    instead of an intermediate step with the File System.
    This should improve performance signifigantly and prevent corruption or filesystem errors.
    
    * ok i guess (#108)
    
    * Pull changes from development to main. (#110)
    
    * ok i guess
    
    * Typo in merge conflict resolution.
    
    * Pull changes from development into main. (#112)
    
    * ok i guess
    
    * Typo in merge conflict resolution.
    
    * yay firebase mimetypes ;kdfghlskgdhliuvhjsry8o6857yteusghkrf
    
    * Pull development changes into main. (#115)
    
    * ok i guess
    
    * Typo in merge conflict resolution.
    
    * yay firebase mimetypes ;kdfghlskgdhliuvhjsry8o6857yteusghkrf
    
    * Begin work on /api/rooms/*
    
    * Formatting and rate limit.
    
    * Room public info fetch support.
    
    Developer only, under development.
    
    * Resolve room public info exception.
    
    * Implemented more room info endpoints.
    
    * /api/rooms/:id/user-permissions
    * /api/rooms/:id/permissions-table
    
    * Added subroom data endpoint.
    
    /api/rooms/:id/subrooms
    
    * Begin work on room session handling.
    
    * Add analytics endpoints for room instances and online players.
    
    * Resolve issue with matchmaking + fix account creation.
    
    * Bump express-rate-limit from 6.2.1 to 6.3.0 (#114)
    
    Bumps [express-rate-limit](https://github.com/nfriedly/express-rate-limit) from 6.2.1 to 6.3.0.
    - [Release notes](https://github.com/nfriedly/express-rate-limit/releases)
    - [Changelog](https://github.com/nfriedly/express-rate-limit/blob/master/changelog.md)
    - [Commits](https://github.com/nfriedly/express-rate-limit/compare/v6.2.1...v6.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: express-rate-limit
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- routers/matchmaking.js
+++ routers/matchmaking.js
@@ -1,14 +1,163 @@
 const router = require('express').Router();
-const helpers = require('../helpers');
-const middleware = require('../middleware');
-const fs = require('fs');
-const BadWordList = JSON.parse(fs.readFileSync('./data/external/badwords-master/array.json'));
-const sanitize = require('sanitize-filename');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
 
 router.get("/*", async (req, res) => {
      res.sendStatus(501);
 });
 
-module.exports = router;
+const MatchmakingModes = {
+     Public: "public",
+     Unlisted: "unlisted",
+     Private: "private",
+     Locked: "locked"
+};
+
+// YES, I'M AWARE
+// The fact that this system is entirely in-memory and therefore volatile is INTENTIONAL.
+// By design, when the API goes down, all users are removed from the game due to their websockets disconnecting.
+// This prevents failed API calls and cheating by faking a websocket connection.
+// HOWEVER - This also means that instances created by Photon will slowly be removed from the room list when all users are disconnected.
+// This means that the RoomSessions dictionary will be OUT OF DATE if it persists.
+// Therefore, it is in memory. This allows for both faster read/writes, better class implementation, more intellisense options,
+// and preventing the aforementioned persistence issues.
+
+// "Well, just make it so Photon Instances never expire, and you can persist as long as you like"
+// THIS IS A WASTE OF RESOURCES. It may also incur additional fees from Photon.
+class RoomSession {
+     // Options:
+     // * public
+     // * unlisted
+     // * private
+     // * locked
+     RoomId = "0"; // The room the instance is in.
+     MatchmakingMode; // How players can access this instance. Private by default, meaning you cannot join unless you have an invite.
+     Players; // The current players inside the instance.
+     MaxPlayers; // The maximum number of players the instance can contain.
+     Age; // Age of the instance.
+     TTL;
+     Persistent;
+     FlaggedForRemoval;
+     
+     InstanceId;
+     JoinCode;
+     
+     
+
+     BeginEventLoop() {
+          setInterval(this.EventLoop, 500);
+          setInterval(this.AutomaticInstanceCleanup, this.TTL);
+     }
+     async EventLoop() {
+          this.Age += 500;
+     }
+     AddPlayer(id) {
+          if(typeof id !== 'string') throw new TypeError("Invalid User ID input in AddPlayer - Parameter 'id' must be a string.");
+          if(!this.Players.includes(id)) this.Players.push(id);
+     }
+     RemovePlayer(id) {
+          if(typeof id !== 'string') throw new TypeError("Invalid User ID input in RemovePlayer - Parameter 'id' must be a string.");
+          while(this.Players.includes(id)) {
+               var index = this.Players.findIndex(item => item == id);
+               this.Players = this.Players.splice(index);
+          }
+     }
+     AutomaticInstanceCleanup() {
+          if(this.Persistent || this.Age < this.TTL) return;
+
+          if(typeof this.Players == 'undefined') this.Players = [];
+          if(this.Players.length < 1) {
+               // Makes an instance unjoinable and begins the process of removing it.
+               this.MatchmakingMode = MatchmakingModes.Locked;
+               this.FlaggedForRemoval = true;
+          }
+     }
+     ManualInstanceCleanup() {
+          // Prevent weird behaviour with undefined instances.
+          if(this.Players.length > 0) return;
+
+          // Makes an instance unjoinable and begins the process of removing it.
+          this.MatchmakingMode = MatchmakingModes.Locked;
+          this.FlaggedForRemoval = true;
+     }
+
+     constructor(RoomId, MatchmakingMode, TTL, Persistent, MaxPlayers) {
+          this.RoomId = RoomId;
+          this.MatchmakingMode = MatchmakingMode;
+          this.Players = [];
+          this.MaxPlayers = MaxPlayers;
+          this.Age = 0;
+          this.TTL = TTL;
+          this.Persistent = Persistent;
+          this.FlaggedForRemoval = false;
+
+          this.InstanceId = uuid.v1();
+          this.JoinCode = `CVR_ROOM.${this.RoomId}.INSTANCE.${this.InstanceId}`;
+
+          setTimeout(() => this.BeginEventLoop(), 50);
+     }
+}
+
+var RoomInstances = {
+     "0": [
+          new RoomSession("0", MatchmakingModes.Locked, 300, true, 20)
+     ]
+};
+
+setInterval(CleanupInstances, 300 * 1000);
+
+async function CleanupInstances() {
+     console.log("Initiating automatic instance cleanup.");
+     var CleanedInstances = 0;
+     for(let RoomIdIndex = 0; RoomIdIndex < Object.keys(RoomInstances).length; RoomIdIndex++) {
+          var RoomId = RoomInstances[RoomIdIndex];
+          RoomId = RoomId.filter(item => {
+               if(item.FlaggedForRemoval) {
+                    console.log(`Removed flagged instance of room ${item.RoomId} with id ${item.InstanceId} and join code ${item.JoinCode}`);
+                    CleanedInstances++;
+                    return false;
+               }
+               return true;
+          });
+     }
+     console.log(`Finished cleanup of instances, cleared ${CleanedInstances} instances in total.`);
+}
+
+module.exports = {
+     router: router,
+     MatchmakingModes: MatchmakingModes,
+     GetInstances: async function GetInstances(RoomId) {
+          if(RoomId = "*") {
+               var instances = [];
+               Object.keys(RoomInstances).forEach(element => {
+                    instances.push(RoomInstances[element]);
+               });
+               return instances;
+          }
+          if(!RoomInstances.includes(RoomId)) return [];
+          return RoomInstances[RoomId];
+     },
+     GetInstanceById: async function GetInstanceById(RoomId, InstanceId) {
+          return RoomInstances[RoomId].find(item => item.InstanceId == InstanceId);
+     },
+     GetInstanceByJoinCode: async function GetInstanceByJoinCode(RoomId, JoinCode) {
+          return RoomInstances[RoomId].find(item => item.JoinCode == JoinCode);
+     },
+     SetInstances: async function SetInstances(RoomId, InstanceList) {
+          RoomInstances[RoomId] = InstanceList;
+     },
+     SetInstance: async function SetInstance(RoomId, InstanceId, Instance) {
+          if(!RoomInstances.includes(RoomId)) RoomInstances[RoomId] = [];
+
+          var index = RoomInstances[RoomId].findIndex(item => item.InstanceId == InstanceId);
+          if(index < 0) RoomInstances[RoomId].push(Instance);
+          else RoomInstances[RoomId][index] = Instance;
+     },
+     CreateInstance: async function CreateInstance(RoomId, MatchmakingMode, TTL, Persistent, MaxPlayers) {
+          if(!RoomInstances.includes(RoomId)) RoomInstances[RoomId] = [];
+
+          const room = new RoomSession(RoomId, MatchmakingMode, TTL, Persistent, MaxPlayers);
+          RoomInstances[RoomId].push(room);
+
+          console.log(`New instance of room ${RoomId} created with InstanceId of ${room.InstanceId} and a join code of ${room.JoinCode}`);
+     }
+};
--- routers/rooms.js
+++ routers/rooms.js
@@ -6,11 +6,174 @@ const BadWordList = JSON.parse(fs.readFileSync('./data/external/badwords-master/
 const sanitize = require('sanitize-filename');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
 
-router.get("/*", async (req, res) => {
-     res.sendStatus(501);
+const OncePerHour = rateLimit.rateLimit({
+     "windowMs": 1 * 60 * 60 * 1000,
+     "max": 1,
+     "message": "This endpoint is limited to only 1 request per hour. Please try again in 60 minutes.",
+     "standardHeaders": true,
+     "legacyHeaders": true
 });
 
+router.post("/create", OncePerHour, middleware.authenticateDeveloperToken, async (req, res) => {
+     try {
+          return res.sendStatus(501);
+     } catch {
+          return res.sendStatus(500);
+     }
+});
 
+router.get("/:id/public-info", middleware.authenticateDeveloperToken, async (req, res) => {
+     try {
+          var {id} = req.params;
+          const playerData = helpers.PullPlayerData(req.user.id);
+
+          try {
+               id = parseInt(id);
+               if(isNaN(id)) return res.status(400).send({message: "Invalid room ID specifed."});
+          } catch {
+               return res.status(500).send({message: "Failed to parse room ID."});
+          }
+
+          const db = require('../index').mongoClient.db(process.env.MONGOOSE_DATABASE_NAME);
+          var collection = db.collection("configuration");
+
+          const RoomCount = await collection.findOne({_id: "RoomCount"}).count;
+          if(id > RoomCount || id < 0) return res.status(400).send({message: "Room does not exist."});
+
+          collection = db.collection("rooms");
+
+          var room = await collection.findOne({_id: id});
+          if(typeof room == 'undefined') return res.status(404).send({message: "Room does not exist or cannot be read from the database."});
+
+          if(room.metadata.permissions[req.user.id] == "owner" || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.public);
+
+          var permission = "everyone";
+          if(room.metadata.permissions.includes(req.user.id)) permission = room.metadata.permissions[req.user.id];
+
+          const playerPermissionsTable = room.metadata.permissionTable[permission];
+
+          if(!playerPermissionsTable.viewInfo) return res.status(403).send({message: "You do not have permission to view information about this room."});
+
+          res.status(200).json(room.public);
+     } catch {
+          return res.sendStatus(500);
+     }
+});
+
+router.get("/:id/user-permissions", middleware.authenticateDeveloperToken, async (req, res) => {
+     try {
+          var {id} = req.params;
+          const playerData = helpers.PullPlayerData(req.user.id);
+
+          try {
+               id = parseInt(id);
+               if(isNaN(id)) return res.status(400).send({message: "Invalid room ID specifed."});
+          } catch {
+               return res.status(500).send({message: "Failed to parse room ID."});
+          }
+
+          const db = require('../index').mongoClient.db(process.env.MONGOOSE_DATABASE_NAME);
+          var collection = db.collection("configuration");
+
+          const RoomCount = await collection.findOne({_id: "RoomCount"}).count;
+          if(id > RoomCount || id < 0) return res.status(400).send({message: "Room does not exist."});
+
+          collection = db.collection("rooms");
+
+          var room = await collection.findOne({_id: id});
+          if(typeof room == 'undefined') return res.status(404).send({message: "Room does not exist or cannot be read from the database."});
+
+          if(room.metadata.permissions[req.user.id] == "owner" || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.metadata.permissions);
+
+          var permission = "everyone";
+          if(room.metadata.permissions.includes(req.user.id)) permission = room.metadata.permissions[req.user.id];
+
+          const playerPermissionsTable = room.metadata.permissionTable[permission];
+
+          if(!playerPermissionsTable.editPermissions) return res.status(403).send({message: "You do not have permission to view information about this room."});
+
+          res.status(200).json(room.metadata.permissions);
+     } catch {
+          res.sendStatus(500);
+     }
+});
+
+router.get("/:id/permissions-table", middleware.authenticateDeveloperToken, async (req, res) => {
+     try {
+          var {id} = req.params;
+          const playerData = helpers.PullPlayerData(req.user.id);
+
+          try {
+               id = parseInt(id);
+               if(isNaN(id)) return res.status(400).send({message: "Invalid room ID specifed."});
+          } catch {
+               return res.status(500).send({message: "Failed to parse room ID."});
+          }
+
+          const db = require('../index').mongoClient.db(process.env.MONGOOSE_DATABASE_NAME);
+          var collection = db.collection("configuration");
+
+          const RoomCount = await collection.findOne({_id: "RoomCount"}).count;
+          if(id > RoomCount || id < 0) return res.status(400).send({message: "Room does not exist."});
+
+          collection = db.collection("rooms");
+
+          var room = await collection.findOne({_id: id});
+          if(typeof room == 'undefined') return res.status(404).send({message: "Room does not exist or cannot be read from the database."});
+
+          if(room.metadata.permissions[req.user.id] == "owner" || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.metadata.permissionTable);
+
+          var permission = "everyone";
+          if(room.metadata.permissions.includes(req.user.id)) permission = room.metadata.permissions[req.user.id];
+
+          const playerPermissionsTable = room.metadata.permissionTable[permission];
+
+          if(!playerPermissionsTable.editPermissions) return res.status(403).send({message: "You do not have permission to view information about this room."});
+
+          res.status(200).json(room.metadata.permissionTable);
+     } catch {
+          res.sendStatus(500);
+     }
+});
+
+router.get("/:id/subrooms", middleware.authenticateDeveloperToken, async (req, res) => {
+     try {
+          var {id} = req.params;
+          const playerData = helpers.PullPlayerData(req.user.id);
+
+          try {
+               id = parseInt(id);
+               if(isNaN(id)) return res.status(400).send({message: "Invalid room ID specifed."});
+          } catch {
+               return res.status(500).send({message: "Failed to parse room ID."});
+          }
+
+          const db = require('../index').mongoClient.db(process.env.MONGOOSE_DATABASE_NAME);
+          var collection = db.collection("configuration");
+
+          const RoomCount = await collection.findOne({_id: "RoomCount"}).count;
+          if(id > RoomCount || id < 0) return res.status(400).send({message: "Room does not exist."});
+
+          collection = db.collection("rooms");
+
+          var room = await collection.findOne({_id: id});
+          if(typeof room == 'undefined') return res.status(404).send({message: "Room does not exist or cannot be read from the database."});
+
+          if(room.metadata.permissions[req.user.id] == "owner" || playerData.private.availableTags.includes("Developer") || playerData.private.availableTags.includes("Community Support Team")) return res.status(200).json(room.metadata.subrooms);
+
+          var permission = "everyone";
+          if(room.metadata.permissions.includes(req.user.id)) permission = room.metadata.permissions[req.user.id];
+
+          const playerPermissionsTable = room.metadata.permissionTable[permission];
+
+          if(!playerPermissionsTable.join) return res.status(403).send({message: "You do not have permission to view information about this room."});
+
+          res.status(200).json(room.metadata.subrooms);
+     } catch {
+          res.sendStatus(500);
+     }
+});
 
 module.exports = router;

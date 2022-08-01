9167378715cc19c391e6f003b762ae5ce221573f
Raven <82341739+bubby932@users.noreply.github.com>
2022-07-31 20:53:44 -0700

    Inventory control admin endpoint

--- routers/dev.js
+++ routers/dev.js
@@ -1,9 +1,40 @@
 const router = require('express').Router();
+const { PullPlayerData, PushPlayerData } = require('../helpers');
 const middleware = require('../middleware');
 
 //Check if a token is valid as developer.
 router.get("/check", middleware.authenticateDeveloperToken, async (req, res) => {
     return res.status(200).send("This token is verified as Developer.");
+});
+
+router.post("/accounts/:id/inventory-item", middleware.authenticateDeveloperToken, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {count} = req.body;
+
+        if(typeof count != 'number') 
+            return res.status(400).json({
+                code: "invalid_input",
+                message: "Parameter `count` not specified or not an integer."
+            });
+
+        let data = await PullPlayerData(id);
+        if(data == null) 
+            return res.status(404).json({
+                code: "not_found",
+                message: "No account exists with that ID."
+            });
+
+        data.econ.inventory[id] = count;
+
+        await PushPlayerData(id, data);
+    } catch (ex) {
+        res.status(500).json({
+            code: "internal_error",
+            message: "An unknown internal server error occured. Please try again later."
+        });
+        throw ex;
+    }
 });
 
 

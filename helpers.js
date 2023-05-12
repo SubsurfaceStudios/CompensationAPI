68f3b18c72fca4a0f1b12b9e197bf04485430e31
Raven <raven@az-raven.com>
2023-05-12 13:04:05 -0700

    JSDoc: Documented `helpers.ArePlayersFriends`.

--- helpers.js
+++ helpers.js
@@ -75,6 +75,12 @@ async function ArePlayersAcquantances(player1, player2) {
           data2.private.acquaintances.includes(player1);
 }
 
+/**
+ * Checks whether one of the players has the other in their friends list.
+ * @param {String} player1 The ID of player A.
+ * @param {String} player2 The ID of player B.
+ * @returns {Boolean} Whether or not the players are friends.
+ */
 async function ArePlayersFriends(player1, player2) {
     var data = await PullPlayerData(player1);
     var data2 = await PullPlayerData(player2);

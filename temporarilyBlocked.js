a39a7dde18b8cb5f9c48c25b1ab2eb7a010d8c62
Raven <raven@az-raven.com>
2023-05-12 14:06:52 -0700

    Admin: Removed `temporarilyBlocked.js`.

--- temporarilyBlocked.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-
-async function temporarilyBlocked(req, res, next) {
-     return res.status(503).send({message: "temporarily_blocked_by_developer"});
-}
-
-module.exports = temporarilyBlocked;

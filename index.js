764f88dae190f1aeef0ca09c371f3011eae49aaa
Raven <raven@subsurface.dev>
2026-09-14 08:11:36 -0700

    Packages: Remove dotenv.
    Signed-off-by: Raven <raven@subsurface.dev>

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const RateLimit = require('express-rate-limit');
--- routers/auth.js
+++ routers/auth.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const router = require('express').Router();
 const middleware = require('../middleware');
 const bcrypt = require('bcryptjs');
